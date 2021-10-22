8d336323f4ac5b55a812a22ee8719810921345c1
89041088851 <bogdan.kosnyrev@outlook.com>
2021-10-22 13:36:03 +0300

    learn js

--- index.js
+++ index.js
@@ -27,3 +27,86 @@ function sequentialSizes(val) {
 
 // Замена цепей If Else переключателем
 
+function chainToSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+
+  switch (val) {
+    case "bob":
+    answer = "Marley";
+    break;
+    case 42:
+    answer = "The Answer";
+    break;
+    case 1:
+    answer = "There is no #1";
+    break;
+    case 99:
+    answer = "Missed me by this much!";
+    break;
+    case 7:
+    answer = "Ate Nine";
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+
+// Возврат логических значений из функций
+
+function isLess(a, b) {
+  // Only change code below this line
+    return a < b;
+  // Only change code above this line
+}
+
+isLess(10, 15);
+
+// Возвращайте ранний шаблон для функций
+
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+if (a < 0 || b < 0) {
+  return undefined;
+}
+
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
+
+// Подсчет Карт
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+    if (card == 2 || card == 3 || card == 4 || card ==5 || card ==6) {
+  	//alert("2 3 4 5 6");
+    count += 1;
+  }
+  else if (card == 7 || card == 8 || card == 9) {
+      count += 0;
+  }
+  else if (card == 10 || card == "J" || card == "Q" || card == "K" || card == "A") {
+      count -= 1;
+  }
+  if (count <= 0) {
+  	return String(count) + " Hold";
+  }
+  else {
+  	return String(count) + " Bet";
+  }
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+// 
