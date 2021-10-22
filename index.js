c67d9b643c8362f458608b0958afb93ac892cb4e
Bogdan <bogdan.kosnyrev@outlook.com>
2021-10-22 12:57:53 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,3 +2,28 @@ function sum (a, b) {
   return a + b;
 }
 sum(5, 8);
+window.alert(sum);
+
+
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val) {
+    case 1:
+    case 2:
+    case 3:
+    answer = "Low";
+    break;
+    case 4:
+    case 5:
+    case 6:
+    answer = 'Mid';
+    break;
+    case 7:
+    case 8:
+    case 9:
+    answer = 'High'
+  }
+
+// Замена цепей If Else переключателем
+
